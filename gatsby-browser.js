a62a7b37ca3eaa845a8d5f09f21d55b5b2cfad4b
anshuman.sinha@criodo.com <anshuman.sinha@criodo.com>
2024-01-15 02:03:17 +0530

    gtm added

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,17 @@ import "./src/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
+
+import React from "react"
+
+export const onClientEntry = () => {
+  // Paste the code for the body here
+  const script = document.createElement("script")
+  script.src = "https://www.googletagmanager.com/ns.html?id=GTM-M3X4XLQR"
+  script.height = 0
+  script.width = 0
+  script.style.display = "none"
+  script.style.visibility = "hidden"
+
+  document.body.appendChild(script)
+}
